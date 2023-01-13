da41aaaed1e4e7b182f5003bb6e03795dbc7b954
root <marcangel@gmail.comm>
2023-01-13 15:50:42 +0800

    [OA]: Adding the correct fonts and colors

---
+++ react/src/components/Main.jsx
@@ -0,0 +1,23 @@
+import "./Main.css";
+export const Main = () => {
+    return (
+        <div className="content-main">
+            <div className="banner">
+                <h1 id="banner-text-first-line">Data to enrich your</h1>
+                <h1 id="second-text-first-line">offshore business</h1>
+            </div>
+
+            <span className="details">
+                <p>
+                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                    qui lorem cupidatat commodo. Elit Sunt. amet fugiat vieniam
+                    occaecat fugiat aliqua.
+                </p>
+            </span>
+            <div className="button-container">
+                <button>Get Started</button>
+                <button>View recent deals</button>
+            </div>
+        </div>
+    );
+};
